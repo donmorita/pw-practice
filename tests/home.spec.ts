4344ae3db5e39fa7ff4e3d0f7e3646d88301de6b
Francisco Mora <fr.morac@duocuc.cl>
2025-01-03 18:16:27 -0300

    Added second test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -2,10 +2,17 @@ import { test, expect } from '@playwright/test';
 
 test.describe('Home', () => {
 
-    test('Open HomePage and verify title', async ({ page }) => {
+    test('Open Home Page and verify title', async ({ page }) => {
         // open url
         await page.goto('https://practice.sdetunicorns.com/');
         // verify title
         await expect(page).toHaveTitle("Practice E-Commerce Site – SDET Unicorns");
+    });
+
+    test('Open About Page and verify title', async ({ page }) => {
+        // open url
+        await page.goto('https://practice.sdetunicorns.com/about/');
+        // verify title
+        await expect(page).toHaveTitle("About – Practice E-Commerce Site");
     });
 });
