4971e0c16b3ca8e4c060ced9c02fe8f5be12cf54
Francisco Mora <fr.morac@duocuc.cl>
2025-01-03 18:09:19 -0300

    Updated first test

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,8 +1,8 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Home', () => {
-    
-    test('Open HomePage and verify title', async ({page}) => {
+
+    test('Open HomePage and verify title', async ({ page }) => {
         // open url
         await page.goto('https://practice.sdetunicorns.com/');
         // verify title
